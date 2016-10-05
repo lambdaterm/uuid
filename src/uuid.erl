34a95dc59efacf789909677e18042c7302c473ac
Alexander Karpich <avkarpich@gmail.com>
2016-10-05 13:02:17 +0500

    Initial commit

---
+++ src/uuid.erl
@@ -0,0 +1,15 @@
+-module(uuid).
+
+-export([
+  generate/0
+]).
+
+-type uuid()::bitstring().
+
+-spec generate() -> uuid().
+generate() ->
+  <<A:32, B:16, C:16, D:16, E:48>> =
+    list_to_binary([trunc(random:uniform() * 255) || _ <- lists:seq(1, 16)]),
+  Res = io_lib:format("~8.16.0b-~4.16.0b-4~3.16.0b-~4.16.0b-~12.16.0b",
+    [A, B, C band 16#0fff, D band 16#3fff bor 16#8000, E]),
+  list_to_binary(Res).
